8bc03011a9d3d42979e4447c24f76f71abbe7805
jsaenzm1 <jsaenzm1@miumg.edu.gt>
2021-10-19 22:22:27 -0600

    Proveedores y Pedidos
    Actualización de Proveedores y creación de pedidos

---
+++ ajax/pedido.ajax.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once "../controladores/pedido.controlador.php";
+require_once "../modelos/pedido.modelo.php";
+
+class AjaxPedido{
+	/*=============================================
+	EDITAR PEDIDO
+	=============================================*/
+	public $idPedido;
+	public function ajaxEditarPedido(){
+		$item = "NO_ORDEN";
+		$valor = $this->idPedido;
+		$respuesta = ControladorPedido::ctrMostrarPedido($item, $valor);
+		echo json_encode($respuesta);
+	}
+
+	public function ajaxDetallePedido(){
+		$item = "NO_ORDEN";
+		$valor = $this->idPedido;
+		$respuesta = ControladorPedido::ctrMostrarPedido($item, $valor);
+		echo json_encode($respuesta);
+	}
+
+	public function ajaxListarProductoPedido(){
+		$item = "NO_ORDEN";
+		$valor = $this->idPedido;
+		$respuesta = ControladorPedido::ctrMostrarListaProducto($item, $valor);
+		echo json_encode($respuesta);
+	}
+
+}
+
+/*=============================================
+EDITAR CLIENTE
+=============================================*/
+if(isset($_POST["idPedido"])){
+	$pedido = new AjaxPedido();
+	$pedido -> idPedido = $_POST["idPedido"];
+	$pedido -> ajaxEditarPedido();
+}
+
+if(isset($_POST["idDetallePedido"])){
+	$pedido = new AjaxPedido();
+	$pedido -> idPedido = $_POST["idDetallePedido"];
+	$pedido -> ajaxDetallePedido();
+}
+
+if(isset($_POST["idListarDetallePedido"])){
+	$pedido = new AjaxPedido();
+	$pedido -> idPedido = $_POST["idListarDetallePedido"];
+	$pedido -> ajaxListarProductoPedido();
+}
---
+++ controladores/pedido.controlador.php
@@ -0,0 +1,221 @@
+<?php
+class ControladorPedido{
+
+	/*=============================================
+	CREAR PEDIDO
+	=============================================*/
+
+	static public function ctrCrearPedido(){
+
+			if(isset($_POST["nuevoProveedorPedido"]) &&
+				 isset($_POST["nuevaFechaPedido"]) &&
+			   isset($_POST["nuevaFechaEstimada"])){
+				   	$tabla = "PEDIDO";
+				   	$datos = array("CODIGO_PROVEEDOR"=>$_POST["nuevoProveedorPedido"],
+								           "FECHA_PEDIDO"=>$_POST["nuevaFechaPedido"],
+								           "FECHA_ESTIMADA"=>$_POST["nuevaFechaEstimada"]);
+
+		   	$respuesta = ModeloPedido::mdlIngresarPedido($tabla, $datos);
+
+		   	if($respuesta == "ok"){
+				echo'<script>
+
+				swal({
+					  type: "success",
+					  title: "El cliente ha sido guardado correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar"
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "pedido";
+								}
+							})
+				</script>';
+			}
+		}
+	}
+
+	/*=============================================
+	MOSTRAR PEDIDO
+	=============================================*/
+	static public function ctrMostrarPedido($item, $valor){
+		$tabla = "PEDIDO";
+		$respuesta = ModeloPedido::mdlMostrarPedido($tabla, $item, $valor);
+		return $respuesta;
+	}
+
+	/*=============================================
+	MOSTRAR LISTA PEDIDO
+	=============================================*/
+	static public function ctrMostrarListaProducto($item, $valor){
+		$tabla = "PEDIDO";
+		$respuesta = ModeloPedido::mdlMostrarListaProducto($tabla, $item, $valor);
+		return $respuesta;
+	}
+
+	/*=============================================
+	EDITAR CLIENTE
+	=============================================*/
+	static public function ctrEditarPedido(){
+
+		if(isset($_POST["editarProveedorPedido"]) &&
+			 isset($_POST["editarFechaPedido"]) &&
+			 isset($_POST["editarFechaEstimada"])
+			){
+
+			if(preg_match('/^[a-zA-Z-ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevoProveedorPedido"])){
+
+				$tabla = "PEDIDO";
+
+				$datos = array("NO_ORDEN"=>$_POST["idPedido"],
+											 "CODIGO_PROVEEDOR"=>$_POST["editarProveedorPedido"],
+											 "FECHA_PEDIDO"=>$_POST["editarFechaPedido"],
+											 "FECHA_ESTIMADA"=>$_POST["editarFechaEstimada"]);
+
+				$respuesta = ModeloPedido::mdlEditarPedido($tabla, $datos);
+				if($respuesta == "ok"){
+					echo'<script>
+					swal({
+						  type: "success",
+						  title: "El cliente ha sido cambiado correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "pedido";
+									}
+								})
+					</script>';
+				}
+			}else{
+				echo'<script>
+					swal({
+						  type: "error",
+						  title: "¡El cliente no puede ir vacío o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "pedido";
+							}
+						})
+			  	</script>';
+			}
+		}
+	}
+
+
+
+	/*=============================================
+	CREAR DETALLE DEL PEDIDO
+	=============================================*/
+	static public function ctrCrearDetallePedido(){
+
+		if(isset($_POST["editarCodigoAsignacionDetalle"]) &&
+		   isset($_POST["editarCantidadDetallePedido"]) &&
+			 isset($_POST["editarPrecioDetallePedido"])
+				){
+
+			if(preg_match('/^[0-9]+$/', $_POST["editarCantidadDetallePedido"]) &&
+		     preg_match('/^[0-9.]+$/', $_POST["editarPrecioDetallePedido"])){
+
+			   	$tabla = "DETALLE_PEDIDO";
+			   	$datos = array("NO_ORDEN"=>$_POST["idDetallePedidoEval"],
+							           "CODIGO_ASIGNACION"=>$_POST["editarCodigoAsignacionDetalle"],
+												 "CANTIDAD"=>$_POST["editarCantidadDetallePedido"],
+												 "PRECIO_UNITARIO"=>$_POST["editarPrecioDetallePedido"]
+											 );
+
+			   	$respuesta = ModeloPedido::mdlIngresarDetallePedido($tabla, $datos);
+
+			   	if($respuesta == "ok"){
+						echo'<script>
+
+						swal({
+							  type: "success",
+							  title: "El producto ha sido agregado al pedido",
+							  showConfirmButton: true,
+							  confirmButtonText: "Cerrar"
+							  }).then(function(result){
+										if (result.value) {
+										window.location = "pedido";
+										}
+									})
+						</script>';
+					}else{
+						echo'<script>
+						swal({
+							  type: "error",
+							  title: "Ocurrió un error en la Base de Datos",
+							  showConfirmButton: true,
+							  confirmButtonText: "pedido"
+							  })
+						</script>';
+					}
+
+			}else{
+				echo'<script>
+					swal({
+						  type: "error",
+						  title: "¡Debe ingresar valores correctos!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+							window.location = "pedido";
+							}
+						})
+			  	</script>';
+			}
+		}
+	}
+
+
+
+
+	/*=============================================
+	ELIMINAR CLIENTE
+	=============================================*/
+	static public function ctrEliminarPedido(){
+
+		if(isset($_GET["idPedido"])){
+			$tabla ="PEDIDO";
+			$datos = $_GET["idPedido"];
+			$respuesta = ModeloPedido::mdlEliminarPedido($tabla, $datos);
+
+			if($respuesta == "ok"){
+				echo'<script>
+				swal({
+					  type: "success",
+					  title: "El cliente ha sido borrado correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar",
+					  closeOnConfirm: false
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "pedido";
+								}
+							})
+
+				</script>';
+
+			}
+		}
+	}
+
+	static public function ctrMostrarProveedorPedido(){
+		$respuesta = ModeloPedido::mdlMostrarProveedoresPedidos();
+		return $respuesta;
+	}
+
+	static public function ctrMostrarProductosPedido(){
+		$respuesta = ModeloPedido::mdlMostrarProductoPedido();
+		return $respuesta;
+	}
+
+
+}
