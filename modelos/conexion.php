c4c888ca9fe73ee5cc000aea9aca8a9b4d918d92
OscarVentura30 <89185365+OscarVentura30@users.noreply.github.com>
2021-11-09 01:30:19 -0600

    unir versiones

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=nissi2",
+		$link = new PDO("mysql:host=localhost;dbname=nissi4",
 			            "root",
 			            "");
 
